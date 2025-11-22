d71fd377ed7c3f0f47f768204469744af0dc7b77
Oleg Latypov <latol179@mail.ru>
2025-11-22 23:55:47 +0300

    settings modifed

--- hexlet_code/task_manager/task_manager/settings.py
+++ hexlet_code/task_manager/task_manager/settings.py
@@ -67,7 +67,7 @@ TEMPLATES = [
         "DIRS": [
             BASE_DIR / "templates/",
         ],
-        "APP_DIRS": True,
+        "APP_DIRS": False,
         "OPTIONS": {
             "context_processors": [
                 "django.template.context_processors.request",
@@ -114,11 +114,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = "ru-ru"
+LANGUAGE_CODE = "en-us"
 
 TIME_ZONE = "UTC"
 
 USE_I18N = True
+
+USE_L10N = True
 
 USE_TZ = True
 
