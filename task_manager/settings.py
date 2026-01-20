99522814f447a13c45b64f4bca927e02517bcbcd
Oleg Latypov <latol179@mail.ru>
2026-01-20 20:27:34 +0300

    modified:   task_manager/settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -183,5 +183,5 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_URL = "login"
-LOGIN_REDIRECT_URL = "/users/"
+LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
