f87730fddc7c1c9e091f3d4605b8c51cbfac7c9e
Oleg Latypov <latol179@mail.ru>
2026-01-21 19:26:32 +0300

    modified:   task_manager/urls.py modified:   task_manager/users/urls.py modified:   templates/base.html modified:   templates/index.html

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,8 +1,8 @@
-
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views  # ← добавили
 from . import views
-from task_manager.users.views import CustomLoginView
+from task_manager.users.views import CustomLogin0View
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -13,4 +13,5 @@ urlpatterns = [
     path("tasks/", include("task_manager.tasks.urls")),
     path("test-rollbar/", views.trigger_error),
     path("login/", CustomLoginView.as_view(), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),  # ← добавили
 ]
--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -1,18 +1,22 @@
-from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from . import views
-
-app_name = 'users'
+from task_manager.users.views import UserCreateView, CustomLoginView
 
 urlpatterns = [
-    path('', views.UserListView.as_view(), name='index'),
-    path('create/', views.UserCreateView.as_view(), name='create'),
-    path('<int:pk>/update/', views.UserUpdateView.as_view(), name='update'),
-    path('<int:pk>/delete/', views.UserDeleteView.as_view(), name='delete'),
-    path('login/',
-         LoginView.as_view(template_name="registration/login.html"),
-         name="login"),
-    path('logout/',
-         LogoutView.as_view(template_name="registration/logout.html"),
-         name='logout'),
-    ]
+    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+
+    # Глобальные маршруты аутентификации
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', UserCreateView.as_view(), name='register'),
+
+    # Остальные приложения
+    path('users/', include('task_manager.users.urls')),
+    path('statuses/', include('task_manager.statuses.urls')),
+    path('labels/', include('task_manager.labels.urls')),
+    path('tasks/', include('task_manager.tasks.urls')),
+    path('test-rollbar/', views.trigger_error),
+]
