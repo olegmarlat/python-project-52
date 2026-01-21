65b43ea2a5a865f5b8de505f6370aeecf68884d4
Oleg Latypov <latol179@mail.ru>
2026-01-21 20:09:51 +0300

    modified:   task_manager/users/urls.py

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -7,14 +7,10 @@ from task_manager.users.views import UserCreateView, CustomLoginView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-
-    # Глобальные маршруты аутентификации
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', UserCreateView.as_view(), name='register'),
-
-    # Остальные приложения
-    path('users/', include('task_manager.users.urls')),
+    # path('users/', include('task_manager.users.urls')),
     path('statuses/', include('task_manager.statuses.urls')),
     path('labels/', include('task_manager.labels.urls')),
     path('tasks/', include('task_manager.tasks.urls')),
