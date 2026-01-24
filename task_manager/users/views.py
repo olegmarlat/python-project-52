a0da890ad5939176f0cad0bcbe303b2580ab6596
Oleg Latypov <latol179@mail.ru>
2026-01-24 20:26:18 +0300

    modified:   task_manager/users/views.py

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -225,6 +225,6 @@ class CustomLogoutView(BaseLogoutView):
         from django.contrib.auth import logout
         logout(request)
         # сообщение об успешном выходе
-        messages.success(request, _("You have been logged out successfully"))
+        messages.success(request, _("Вы успешно вышли из системы!"))
         # переход на главную
         return redirect('/')
