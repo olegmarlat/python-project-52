c60a950c8ba868db96b1ca47b33ab3c2bf1c4089
Oleg Latypov <latol179@mail.ru>
2026-01-26 23:14:13 +0300

    modified:   task_manager/statuses/urls.py

--- task_manager/statuses/urls.py
+++ task_manager/statuses/urls.py
@@ -6,6 +6,6 @@ app_name = 'statuses'
 urlpatterns = [
     path("", views.statuses_list, name="statuses_list"),
     path("create/", views.status_create, name="status_create"),
-    path("update/<int:pk>/", views.status_update, name="status_update"),
-    path("delete/<int:pk>/", views.status_delete, name="status_delete"),
+    path("<int:pk>/update/", views.status_update, name="status_update"),
+    path("<int:pk>/delete/", views.status_delete, name="status_delete"),
 ]
