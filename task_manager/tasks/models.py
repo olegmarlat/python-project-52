b075ef6e50bba5e3883161fc344ca236df4ecf30
Oleg Latypov <latol179@mail.ru>
2026-01-27 22:21:44 +0300

    modified:   task_manager/tasks/models.py

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -1,3 +1,4 @@
+# task_manager/tasks/models.py
 from django.db import models
 from django.conf import settings
 from task_manager.labels.models import Label
@@ -10,10 +11,10 @@ class Task(models.Model):
         "statuses.Status", on_delete=models.PROTECT, verbose_name="Статус"
     )
     author = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="created_tasks"
     )
-    labels = models.ManyToManyField(Label, blank=True)
-    
     executor = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.PROTECT,
@@ -23,7 +24,6 @@ class Task(models.Model):
         verbose_name="Исполнитель"
     )
     labels = models.ManyToManyField(Label, blank=True, verbose_name="Метки")
-
 
     def __str__(self):
         return self.name
