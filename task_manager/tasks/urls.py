a0af35fa94cb38daa1c5ac8952a86e0bc567af50
Oleg Latypov <latol179@mail.ru>
2026-01-28 00:05:08 +0300

    modified:   task_manager/tasks/urls.py

--- task_manager/tasks/urls.py
+++ task_manager/tasks/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.TaskListView.as_view(), name="tasks_list"),
     path("create/", views.task_create, name="task_create"),
-    path("update/<int:pk>/", views.task_update, name="task_update"),
-    path("delete/<int:pk>/", views.task_delete, name="task_delete"),
+    path("<int:pk>/update/", views.task_update, name="task_update"),
+    path("<int:pk>/delete/", views.task_delete, name="task_delete"),
 ]
