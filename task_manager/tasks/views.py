aad27f1898762bd768df6747c005c46c7dcf3ef5
Oleg Latypov <latol179@mail.ru>
2026-01-27 23:09:17 +0300

    modified:   task_manager/tasks/views.py

--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -16,8 +16,9 @@ def task_create(request):
             task = form.save(commit=False)
             task.author = request.user
             task.save()
+            form.save_m2m()
             messages.success(request, "Задача успешно создана!")
-            return redirect("tasks:tasks_list")
+            return redirect("tasks_list")
     else:
         form = TaskForm()
     return render(request, "tasks/task_form.html", {"form": form})
@@ -28,13 +29,13 @@ def task_update(request, pk):
     task = get_object_or_404(Task, pk=pk)
     if task.author != request.user:
         messages.error(request, "Вы не можете редактировать чужую задачу.")
-        return redirect("tasks:tasks_list")
+        return redirect("tasks_list")
     if request.method == "POST":
         form = TaskForm(request.POST, instance=task)
         if form.is_valid():
             form.save()
             messages.success(request, "Задача успешно изменена!")
-            return redirect("tasks:tasks_list")
+            return redirect("tasks_list")
     else:
         form = TaskForm(instance=task)
     return render(request, "tasks/task_form.html", {"form": form})
@@ -45,11 +46,11 @@ def task_delete(request, pk):
     task = get_object_or_404(Task, pk=pk)
     if task.author != request.user:
         messages.error(request, "Вы не можете удалить чужую задачу!")
-        return redirect("tasks:tasks_list")
+        return redirect("tasks_list")
     if request.method == "POST":
         task.delete()
         messages.success(request, "Задача успешно удалена!")
-        return redirect("tasks:tasks_list")
+        return redirect("tasks_list")
     return render(request, "tasks/task_confirm_delete.html", {"task": task})
 
 
