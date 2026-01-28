d2857d6f18959b1c6fe71f28651a9d31fdb8e2fd
Oleg Latypov <latol179@mail.ru>
2026-01-29 00:05:22 +0300

    modified:   task_manager/urls.py

--- task_manager/statuses/urls.py
+++ task_manager/statuses/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'statuses'
 
 urlpatterns = [
-    path("", views.statuses_list, name="statuses_list"),
-    path("create/", views.status_create, name="status_create"),
-    path("<int:pk>/update/", views.status_update, name="status_update"),
-    path("<int:pk>/delete/", views.status_delete, name="status_delete"),
+    path("", views.statuses_list, name="index"),  # ← index вместо statuses_list
+    path("create/", views.status_create, name="create"),
+    path("<int:pk>/update/", views.status_update, name="update"),
+    path("<int:pk>/delete/", views.status_delete, name="delete"),
 ]
--- task_manager/tasks/urls.py
+++ task_manager/tasks/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from . import views
+from .views import (
+    TaskListView,
+    task_create,
+    task_update,
+    task_delete,
+)
 
 urlpatterns = [
-    path("", views.TaskListView.as_view(), name="tasks_list"),
-    path("create/", views.task_create, name="task_create"),
-    path("<int:pk>/update/", views.task_update, name="task_update"),
-    path("<int:pk>/delete/", views.task_delete, name="task_delete"),
+    path("", TaskListView.as_view(), name="index"),  # ← index вместо tasks_list
+    path("create/", task_create, name="create"),
+    path("<int:pk>/update/", task_update, name="update"),
+    path("<int:pk>/delete/", task_delete, name="delete"),
 ]
