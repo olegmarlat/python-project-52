5c62e3bb002b571c547b241b54e3f4d3a3b7580f
Oleg Latypov <latol179@mail.ru>
2026-02-07 20:29:08 +0300

    modified:   task_manager/users/apps.py

--- task_manager/users/apps.py
+++ task_manager/users/apps.py
@@ -1,4 +1,13 @@
 from django.apps import AppConfig
+from django.contrib.auth.signals import user_logged_in
+from django.contrib.messages import success
+from django.dispatch import receiver
+from django.utils.translation import gettext_lazy as _
+
+
+@receiver(user_logged_in)
+def show_login_message(sender, request, user, **kwargs):
+    success(request, _("Вы залогинены"))
 
 
 class UsersConfig(AppConfig):
