1b07f10c05ffb49668307a1ba0b5dca56971b4e5
olegmarlat <99600983+olegmarlat@users.noreply.github.com>
2026-02-09 13:39:17 +0300

    Update models.py

--- task_manager/statuses/models.py
+++ task_manager/statuses/models.py
@@ -1,10 +1,23 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
-# Create your models here.
-class Status(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+class TaskStatus(models.Model):
+    """Модель статуса задачи"""
+    name = models.CharField(
+        max_length=150,
+        unique=True,
+        verbose_name=_('Имя')
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name=_('Дата создания')
+    )
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = _('Статус задачи')
+        verbose_name_plural = _('Статусы задач')
+        ordering = ['-created_at']
