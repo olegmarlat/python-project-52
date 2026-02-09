b8ec45b6d3325e27374879f580d86ba4f1fb4d3d
olegmarlat <99600983+olegmarlat@users.noreply.github.com>
2026-02-09 12:43:04 +0300

    Update urls.py

--- task_manager/statuses/urls.py
+++ task_manager/statuses/urls.py
@@ -5,7 +5,8 @@ app_name = 'statuses'
 
 urlpatterns = [
     path("", views.statuses_list, name="statuses_list"),
-    path("create/", views.status_create, name="create"),
+    # path("create/", views.status_create, name="create"),
+    path('create/', StatusCreateView.as_view(), name='status_create'),
     path("<int:pk>/update/", views.status_update, name="update"),
     path("<int:pk>/delete/", views.status_delete, name="delete"),
 ]
