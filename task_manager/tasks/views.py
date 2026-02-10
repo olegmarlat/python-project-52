69206b55e003b2218481efb8757cab5e1d8edfc7
olegmarlat <99600983+olegmarlat@users.noreply.github.com>
2026-02-10 12:55:42 +0300

    Update views.py

--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -6,10 +6,14 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Task
 
 
-class TaskListView(LoginRequiredMixin, ListView):
+class TaskListView(LoginRequiredMixin, FilterView):
     model = Task
     template_name = 'tasks/task_list.html'
     context_object_name = 'tasks'
+    filterset_class = TaskFilter 
+
+    def get_queryset(self):
+        return Task.objects.all()
 
 
 class TaskCreateView(LoginRequiredMixin, CreateView):
