a47cd980973a33f178a727d580da3d8fb9fb61ed
olegmarlat <99600983+olegmarlat@users.noreply.github.com>
2026-02-10 09:55:11 +0300

    Update views.py

--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -1,75 +1,47 @@
-from django.shortcuts import render, get_object_or_404, redirect
+# task_manager/tasks/views.py
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
-from .models import Task
-from .forms import TaskForm
-from django.views.generic import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .filters import TaskFilter
-
-
-@login_required
-def task_create(request):
-    if request.method == "POST":
-        form = TaskForm(request.POST)
-        if form.is_valid():
-            task = form.save(commit=False)
-            task.author = request.user
-            task.save()
-            form.save_m2m()
-            messages.success(request, "Задача успешно создана!")
-            return redirect("tasks_list")
-    else:
-        form = TaskForm()
-    return render(request, "tasks/task_form.html", {"form": form})
-
-
-@login_required
-def task_update(request, pk):
-    task = get_object_or_404(Task, pk=pk)
-    if task.author != request.user:
-        messages.error(request, "Задачу может изменить только ее автор")
-        return redirect("tasks_list")
-    if request.method == "POST":
-        form = TaskForm(request.POST, instance=task)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Задача успешно изменена!")
-            return redirect("tasks_list")
-    else:
-        form = TaskForm(instance=task)
-    return render(request, "tasks/task_form.html", {"form": form})
-
-
-@login_required
-def task_delete(request, pk):
-    task = get_object_or_404(Task, pk=pk)
-    if task.author != request.user:
-        messages.error(request, "Задачу может удалить только ее автор")
-        return redirect("tasks_list")
-    if request.method == "POST":
-        task.delete()
-        messages.success(request, "Задача успешно удалена!")
-        return redirect("tasks_list")
-    return render(request, "tasks/task_confirm_delete.html", {"task": task})
+from .models import Task
 
 
 class TaskListView(LoginRequiredMixin, ListView):
     model = Task
-    template_name = "tasks/task_list.html"
-    context_object_name = "tasks"
-    ordering = ["-id"]
+    template_name = 'tasks/task_list.html'
+    context_object_name = 'tasks'
 
-    def get_queryset(self):
-        queryset = Task.objects.all()
-        self.filterset = TaskFilter(
-            self.request.GET,
-            queryset=queryset,
-            request=self.request,
-        )
-        return self.filterset.qs
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["filterset"] = self.filterset
-        return context
+class TaskCreateView(LoginRequiredMixin, CreateView):
+    model = Task
+    template_name = 'tasks/task_form.html'
+    fields = ['name', 'description', 'status', 'executor', 'labels']
+    success_url = reverse_lazy('tasks:task_list')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(self.request, 'Задача успешно создана')
+        return response
+
+
+class TaskUpdateView(LoginRequiredMixin, UpdateView):
+    model = Task
+    template_name = 'tasks/task_form.html'
+    fields = ['name', 'description', 'status', 'executor', 'labels']
+    success_url = reverse_lazy('tasks:task_list')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(self.request, 'Задача успешно изменена')
+        return response
+
+
+class TaskDeleteView(LoginRequiredMixin, DeleteView):
+    model = Task
+    template_name = 'tasks/task_confirm_delete.html'
+    success_url = reverse_lazy('tasks:task_list')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(self.request, 'Задача успешно удалена')
+        return response
