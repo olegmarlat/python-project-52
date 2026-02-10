3711b59f9ab388223f59aa527162fd24ad00b085
olegmarlat <99600983+olegmarlat@users.noreply.github.com>
2026-02-10 09:35:16 +0300

    Update urls.py

--- task_manager/tasks/urls.py
+++ task_manager/tasks/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 from .views import (
     TaskListView,
-    task_create,
+    TaskCreateView,
     task_update,
     task_delete,
 )
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="tasks_list"),
-    path("create/", task_create, name="create"),
+    path("create/", views.TaskCreateView.as_view(), name="create"),
     path("<int:pk>/update/", task_update, name="update"),
     path("<int:pk>/delete/", task_delete, name="delete"),
 ]
