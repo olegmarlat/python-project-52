ef34f9b2bcfc6e6fc0079cc7a0b35a99b6c67a11
Oleg Latypov <latol179@mail.ru>
2026-02-12 00:12:14 +0300

    modified:   task_manager/tasks/forms.py

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -1,8 +1,16 @@
 from django import forms
 from .models import Task
+from task_manager.labels.models import Label
 
 
 class TaskForm(forms.ModelForm):
+    labels = forms.ModelMultipleChoiceField(
+        queryset=Label.objects.all(),
+        widget=forms.SelectMultiple(attrs={'class': 'form-select'}),
+        required=False,
+        label="Метки"
+    )
+
     class Meta:
         model = Task
         fields = ["name", "description", "status", "executor", "labels"]
