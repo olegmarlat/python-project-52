7c0561301153f8966f626f467d49f4b9d4760d9b
olegmarlat <99600983+olegmarlat@users.noreply.github.com>
2026-02-12 11:15:02 +0300

    Update views.py

--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -4,15 +4,14 @@ from django.urls import reverse_lazy
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Task
-from django_filters.views import FilterView
+from django_filters.views import ListView
 from .filters import TaskFilter
 
 
-class TaskListView(LoginRequiredMixin, FilterView):
+class TaskListView(LoginRequiredMixin, ListView):
     model = Task
     template_name = 'tasks/task_list.html'
     context_object_name = 'tasks'
-    filterset_class = TaskFilter
 
     def get_queryset(self):
         return Task.objects.all()
