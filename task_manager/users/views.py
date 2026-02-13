14c9802ff5029138f09225ac53b4192ad3477373
olegmarlat <99600983+olegmarlat@users.noreply.github.com>
2026-02-13 13:54:27 +0300

    Update views.py

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -110,7 +110,7 @@ class UserDeleteView(
     DeleteView
 ):
     model = User
-    template_name = "users/user_confirm_delete.html"  # Это важно!
+    template_name = "users/user_delete.html"
     success_url = reverse_lazy("users:index")
     success_message = _("Пользователь успешно удален")
 
