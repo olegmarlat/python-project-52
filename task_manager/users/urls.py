f2dab13ddc376374ca49b52c3595509ed7c11c6a
olegmarlat <99600983+olegmarlat@users.noreply.github.com>
2026-02-13 11:34:44 +0300

    Update urls.py

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('create/', views.UserCreateView.as_view(), name='user_create'),
     # path('create/', views.UserCreateView.as_view(), name='create'),
     path('<int:pk>/update/', views.UserUpdateView.as_view(), name='update'),
-    path('<int:pk>/delete/', views.UserDeleteView.as_view(), name='delete'),
+    # path('<int:pk>/delete/', views.UserDeleteView.as_view(), name='delete'),
+    path('<int:pk>/delete/', UserDeleteView.as_view(), name='delete')
 ]
