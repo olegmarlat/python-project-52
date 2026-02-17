9f1ff86d22d9ca0e266722b63c1da23de64a4514
olegmarlat <99600983+olegmarlat@users.noreply.github.com>
2026-02-17 10:48:41 +0300

    Update views.py

--- task_manager/labels/views.py
+++ task_manager/labels/views.py
@@ -1,45 +1,40 @@
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.db.models import ProtectedError
+from django.shortcuts import redirect
 from .models import Label
 
-
-class LabelListView(ListView):
+class LabelListView(LoginRequiredMixin, ListView):
     model = Label
     template_name = 'labels/label_list.html'
     context_object_name = 'labels'
 
-
-class LabelCreateView(CreateView):
+class LabelCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
     model = Label
     template_name = 'labels/label_form.html'
     fields = ['name']
     success_url = reverse_lazy('labels:label_list')
+    success_message = 'Метка успешно создана'
 
-    def form_valid(self, form):
-        response = super().form_valid(form)
-        messages.success(self.request, 'Метка успешно создана')
-        return response
-
-
-class LabelUpdateView(UpdateView):
+class LabelUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     model = Label
     template_name = 'labels/label_form.html'
     fields = ['name']
     success_url = reverse_lazy('labels:label_list')
+    success_message = 'Метка успешно изменена'
 
-    def form_valid(self, form):
-        response = super().form_valid(form)
-        messages.success(self.request, 'Метка успешно изменена')
-        return response
-
-
-class LabelDeleteView(DeleteView):
+class LabelDeleteView(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
     model = Label
     template_name = 'labels/label_delete.html'
     success_url = reverse_lazy('labels:label_list')
+    success_message = 'Метка успешно удалена'
 
-    def form_valid(self, form):
-        response = super().form_valid(form)
-        messages.success(self.request, 'Метка успешно удалена')
-        return response
+    def post(self, request, *args, **kwargs):
+        try:
+            return super().post(request, *args, **kwargs)
+        except ProtectedError:
+            messages.error(self.request, 'Невозможно удалить метку, потому что она используется')
+            return redirect('labels:label_list')
