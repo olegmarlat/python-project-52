03d0190ea3f77608df23cb3a3807a86c3c376999
olegmarlat <99600983+olegmarlat@users.noreply.github.com>
2026-02-17 12:24:55 +0300

    Update views.py

--- task_manager/labels/views.py
+++ task_manager/labels/views.py
@@ -3,14 +3,15 @@ from django.urls import reverse_lazy
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
-from django.db.models import ProtectedError
 from django.shortcuts import redirect
 from .models import Label
+
 
 class LabelListView(LoginRequiredMixin, ListView):
     model = Label
     template_name = 'labels/label_list.html'
     context_object_name = 'labels'
+
 
 class LabelCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
     model = Label
@@ -19,12 +20,14 @@ class LabelCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
     success_url = reverse_lazy('labels:label_list')
     success_message = 'Метка успешно создана'
 
+
 class LabelUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     model = Label
     template_name = 'labels/label_form.html'
     fields = ['name']
     success_url = reverse_lazy('labels:label_list')
     success_message = 'Метка успешно изменена'
+
 
 class LabelDeleteView(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
     model = Label
