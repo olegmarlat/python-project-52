120b4926f705b466e2e1b188b913b4bca71658d5
olegmarlat <99600983+olegmarlat@users.noreply.github.com>
2026-02-17 12:30:56 +0300

    Update views.py

--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.core.exceptions import PermissionDenied
 from django_filters.views import FilterView
 from django.views.generic import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
