2053b94a8ca48fbfbf71db5fd351a58bf7ad6aa9
olegmarlat <99600983+olegmarlat@users.noreply.github.com>
2026-02-24 11:31:49 +0300

    Update forms.py

--- task_manager/statuses/forms.py
+++ task_manager/statuses/forms.py
@@ -1,14 +1,17 @@
-<!-- templates/statuses/status_form.html -->
-{% extends "base.html" %}
-{% load django_bootstrap5 %}
+from django import forms
+from .models import Status
 
-{% block content %}
-  <h1 class="my-4">{% if form.instance.pk %}Редактировать статус{% else %}Новый статус{% endif %}</h1>
-  
-  <form method="post">
-    {% csrf_token %}
-    {% bootstrap_form form %}
-    {% bootstrap_button button_type="submit" content="Создать" button_class="btn-primary" %}
-    <a href="{% url 'status_list' %}" class="btn btn-secondary">Отмена</a>
-  </form>
-{% endblock %}
+
+class StatusForm(forms.ModelForm):
+    class Meta:
+        model = Status
+        fields = ["name"]
+        labels = {
+            "name": "Имя",
+        }
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Введите имя статуса'
+            }),
+        }
