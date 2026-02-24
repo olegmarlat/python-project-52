c734ad042dead997c9ae898cf6206d0baf6819b4
Oleg Latypov <latol179@mail.ru>
2026-02-24 21:04:06 +0300

    modified:   task_manager/statuses/views.py

--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -1,12 +1,9 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import redirect
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib import messages
 from django.db.models import ProtectedError
 from .models import Status
-from .forms import StatusForm
-from django.contrib.auth.decorators import login_required
-
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
@@ -19,9 +16,9 @@ class StatusListView(ListView):
 
 class StatusCreateView(CreateView):
     model = Status
-    template_name = 'statuses/status_form.html'
+    template_name = 'statuses/status_create.html'
     fields = ['name']
-    success_url = reverse_lazy('statuses_list')
+    success_url = reverse_lazy('status_list')
 
     def form_valid(self, form):
         response = super().form_valid(form)
@@ -31,9 +28,9 @@ class StatusCreateView(CreateView):
 
 class StatusUpdateView(UpdateView):
     model = Status
-    template_name = 'statuses/status_form.html'
+    template_name = 'statuses/status_update.html'
     fields = ['name']
-    success_url = reverse_lazy('statuses_list')
+    success_url = reverse_lazy('status_list')
 
     def form_valid(self, form):
         response = super().form_valid(form)
@@ -44,7 +41,7 @@ class StatusUpdateView(UpdateView):
 class StatusDeleteView(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
     model = Status
     template_name = 'statuses/status_confirm_delete.html'
-    success_url = reverse_lazy('statuses_list')
+    success_url = reverse_lazy('status_list')
 
     def post(self, request, *args, **kwargs):
         try:
@@ -56,55 +53,4 @@ class StatusDeleteView(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
                 self.request,
                 "Невозможно удалить статус, потому что он используется"
             )
-            return redirect('statuses_list')
-
-
-@login_required
-def statuses_list(request):
-    statuses = Status.objects.all()
-    return render(
-        request, "statuses/statuses_list.html", {"statuses": statuses}
-    )
-
-
-@login_required
-def status_create(request):
-    if request.method == "POST":
-        form = StatusForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Статус успешно создан!")
-            return redirect("statuses_list")
-    else:
-        form = StatusForm()
-    return render(request, "statuses/status_form.html", {"form": form})
-
-
-@login_required
-def status_update(request, pk):
-    status = get_object_or_404(Status, pk=pk)
-    if request.method == "POST":
-        form = StatusForm(request.POST, instance=status)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Статус успешно изменен!")
-            return redirect("statuses_list")
-    else:
-        form = StatusForm(instance=status)
-    return render(request, "statuses/status_form.html", {"form": form})
-
-
-@login_required
-def status_delete(request, pk):
-    status = get_object_or_404(Status, pk=pk)
-    if request.method == "POST":
-        try:
-            status.delete()
-            messages.success(request, "Статус успешно удален!")
-            return redirect("statuses_list")
-        except ProtectedError:
-            messages.error(request, "Невозможно удалить статус")
-            return redirect("statuses_list")
-    return render(
-        request, "statuses/status_confirm_delete.html", {"status": status}
-    )
+            return redirect('status_list')
